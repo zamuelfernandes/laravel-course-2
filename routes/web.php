777317470a23a722d7370618c702771e427e9912
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-07-26 08:38:43 -0300

    Navigation Link and Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories', \App\Http\Controllers\CategoryController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
